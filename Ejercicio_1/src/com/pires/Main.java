be866bf8a80d14c1949f852d2ef9d5a5c7954b4b
Santiago Pires Rodríguez <85551119+SantiagoPiresDev@users.noreply.github.com>
2021-12-03 11:54:12 +0100

    Add files via upload

---
+++ Ejercicio_1/src/com/pires/Main.java
@@ -0,0 +1,30 @@
+package com.pires;
+
+public class Main {
+
+    public static void main(String[] args)
+        {
+            // Este código imprime todos los tipos de datos vistos
+            byte tipoByte = 0;
+            int tipoInt = 1;
+            short tipoShort = 3;
+            long tipoLong = 4;
+            float tipoFloat = 5.0f;
+            double tipoDouble = 6.0d;
+            char tipoChar = 'a';
+            boolean tipoBoolean = false;
+            String tipoString ="Hola Mundo, la frase más común en el mundo de la programación";
+
+            System.out.println("Valor del tipo Byte: " + tipoByte);
+            System.out.println("Valor del tipo Int: " + tipoInt);
+            System.out.println("Valor del tipo Short: " + tipoShort);
+            System.out.println("Valor del tipo Long: " + tipoLong);
+            System.out.println("Valor del tipo Float: " + tipoFloat);
+            System.out.println("Valor del tipo Double: " + tipoDouble);
+            System.out.println("Valor del tipo Char: " + tipoChar);
+            System.out.println("Valor del tipo Boolean: " + tipoBoolean);
+            System.out.println("Valor del tipo String: " + tipoString);
+
+        }
+}
+
